1b2460fdbb2008414cf0c820026ca21583f9b490
Ayanrajpoot10 <Ayanrajpoot2004@gmail.com>
2024-09-25 16:46:00 +0530

    Fixed module path to bughunter-go

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/aztecrabbit/bugscanner-go
+module github.com/Ayanrajpoot10/bugscanner-go
 
 go 1.18
 
--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "github.com/aztecrabbit/bugscanner-go/cmd"
+import (
+    "github.com/Ayanrajpoot10/bugscanner-go/cmd"
+    "github.com/fatih/color"
+)
 
 func main() {
-	cmd.Execute()
+    welcomeColor := color.New(color.FgGreen, color.Bold)
+
+    welcomeColor.Println("Welcome to Bughunter-Go! Modified version of bugscanner-go with fixes. By:- Ayan Rajpoot ,join telegram bugScanX..")
+
+    cmd.Execute()
 }
